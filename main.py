d39385b30cb8e76642adfd77c78f8429d8bcb091
cruld <cruldra@gmail.com>
2024-12-07 04:35:29 +0800

    ✨ feat(main.py): 添加获取元素父元素的函数并更新数字人视频请求参数

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from typing import Optional
 import subprocess
 import os
 import uvicorn
-from playwright.async_api import async_playwright
+from playwright.async_api import async_playwright, ElementHandle
 
 # 创建 FastAPI 实例
 app = FastAPI(
@@ -48,6 +48,7 @@ class ChromeManager:
             self._chrome_process.wait()
             self._chrome_process = None
 
+
 class CreateDigitalVideoRequest(BaseModel):
     """
     使用heygen生成数字人视频时的请求参数
@@ -55,13 +56,15 @@ class CreateDigitalVideoRequest(BaseModel):
     Attributes:
         script_content (str): 脚本内容
         script_locale (str): 脚本语言
-        digital_human_id (str): 数字人ID
+        avatar_name (str): 数字人头像名称
+        look_name (str): 数字人外观名称
         audio_id (str): 音频ID
         size (str): 大小
     """
     script_content: str
     script_locale: str
-    digital_human_id: str
+    avatar_name: str
+    look_name: str
     audio_id: str
     size: str
 
@@ -69,7 +72,15 @@ class CreateDigitalVideoRequest(BaseModel):
 # 在应用启动时启动Chrome
 chrome_manager = ChromeManager.get_instance()
 chrome_manager.start_chrome()
-
+async def get_parent_element(element_handle:ElementHandle):
+    """
+    获取元素的父元素
+    Args:
+        element_handle: ElementHandle对象
+    Returns:
+        ElementHandle: 父元素的ElementHandle对象
+    """
+    return await element_handle.evaluate_handle("element => element.parentElement")
 
 
 @app.post("/digital-video/")
@@ -77,18 +88,36 @@ async def create_digital_video(request: CreateDigitalVideoRequest):
     async with async_playwright() as p:
         browser = await p.chromium.connect_over_cdp("http://localhost:9222")
         context = browser.contexts[0]
-        page =await context.new_page()
+        page = await context.new_page()
         await page.goto('https://app.heygen.com/create-v3/draft?vt=p')
-        time.sleep(3)
+        # 选择头像
+        avatar_element = await page.wait_for_selector(f'div[draggable="false"]>div:nth-child(3)>div>label[title="{request.avatar_name}"]')
+        parent1 = await get_parent_element(avatar_element)
+        parent2 = await get_parent_element(parent1)
+        avatar_container = await get_parent_element(parent2)
+        await avatar_container.click()
+
+        # 选择外观
+        look_element = await page.wait_for_selector(f'div[data-active="false"][draggable="true"] div.css-1xfwczf[title="{request.look_name}"]')
+        parent1 = await get_parent_element(look_element)
+        parent2 = await get_parent_element(parent1)
+        look_container = await get_parent_element(parent2)
+        await look_container.click()
+
+        time.sleep(30)
         await page.close()
     return "ok"
+
+
 def cleanup():
     chrome_manager = ChromeManager.get_instance()
     chrome_manager.close_chrome()
 
+
 def signal_handler(signum, frame):
     cleanup()
     exit(0)
+
 
 if __name__ == '__main__':
     # 注册清理函数
